ad841b734793062f41dd55b69a9d8552fcfe4726
Uday <udayrana428@gmail.com>
2021-11-17 23:05:53 +0530

    created login endpoint and send authtoken to user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -44,4 +44,39 @@ router.post('/createUser', [
     res.status(500).send("some error occured!")
   }
 })
+// create a login endpoint using POST: "/api/auth/login" login required
+router.post('/login', [
+  body('email','Enter a valid email').isEmail(),
+  body('password','Enter correct password').isLength({ min: 5 }),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {email, password} = req.body
+  try {
+    let user = await User.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ errors: "Please try to login with correct credentials" })
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ errors: "Please try to login with correct credentials" })
+    }
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data, jwt_secret);
+    res.json({authtoken})
+  }
+  catch (error) {
+    res.status(500).send("Internal server error")
+  }
+
+})
+let a1="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE5NTBkZDU2ZDJiYTgwNjFmYjgyMjkwIn0sImlhdCI6MTYzNzE1ODM1N30.hTDalIdc9-16wHXuwwmwTClx4O2kmT2AuqwcJg8_Y9g"
+let a2="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjE5NTBkZDU2ZDJiYTgwNjFmYjgyMjkwIn0sImlhdCI6MTYzNzE1ODQ5Mn0.3H9eLvOAoDy5aO2d3nGZyikRVxxS-hu3svjIe5QRSmA"
+console.log(a1==a2)
 module.exports = router
