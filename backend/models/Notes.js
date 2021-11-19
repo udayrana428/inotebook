5474a59df6340acedbc62778dfa7fa036e9f40b8
Uday <udayrana428@gmail.com>
2021-11-19 16:54:35 +0530

    created fetching, creating notes endpoints of user

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const notesSchema = new Schema({
+  user:{type:mongoose.Schema.Types.ObjectId,
+  ref:"user"},
   title: { type: String, required: true },
   description: { type: String },
   tag: { type: String },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
 });
-const notes = mongoose.model('notes', notesSchema)
-module.exports = notes
+const notes = mongoose.model("notes", notesSchema);
+module.exports = notes;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,42 +1,48 @@
 const express = require("express")
-const Notes=require("../models/Notes")
+const Notes = require("../models/Notes")
 const router = express.Router()
+const fetchuser = require("../middleware/getuser")
 const { body, validationResult } = require('express-validator');
+//Get all the notes using GET: "/api/"
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id })
+  res.json(notes)
+})
 // create notes using POST: "/api/users/createNotes" 
-router.post('/createNotes', [
-    body('title', 'Enter a valid title').isLength({min:2,max:15}),
-    body('description', 'Enter a valid description').isLength({  max: 400 }),
-], async(req, res) => {
-    // if there are errors returns bad errors and the request
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  try{
+router.post('/createNotes',fetchuser, [
+  body('title', 'Enter a valid title').isLength({ min: 2, max: 15 }),
+  body('description', 'Enter a valid description').isLength({ max: 400 }),
+], async (req, res) => {
 
-    // check whether the user with the same email exist
-    let notes = await Notes.findOne({ title: req.body.title })
-    if (notes) {
-      return res.status(400).json({ errors: "Sorry, a document with same title already exist." })
+  try {
+    const { title, description, tag } = req.body
+    // if there are errors returns bad errors and the request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    // create new user
-    notes = await Notes.create({
-      title: req.body.title,
-      description: req.body.description,
-      tag: req.body.tag,
+    const notes=new Notes({
+      title,description,tag,user:req.user.id
     })
-  
-      // .then(user => res.json(user))
-      // .catch(err => {
-      //   console.log(err)
-      //   res.json({ error: "please enter a unique value for email" + err.name, message: err.message })
-      // })
-      res.json({"Nice":"nice"})
+    const savednote=await notes.save()
+    res.json({savednote})
+    // // check whether the user with the same email exist
+    // let notes = await Notes.findOne({ title: req.body.title })
+    // if (notes) {
+    //   return res.status(400).json({ errors: "Sorry, a document with same title already exist." })
+    // }
+    // // create new user
+    // notes = await Notes.create({
+    //   title: req.body.title,
+    //   description: req.body.description,
+    //   tag: req.body.tag,
+    // })
+    // res.json({ "Nice": "nice" })
   }
-  catch(error){
+  catch (error) {
     console.error(error.message)
     res.status(500).send("some error occured!")
   }
-    
+
 })
 module.exports = router
